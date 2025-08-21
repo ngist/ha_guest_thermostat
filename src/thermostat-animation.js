4eaba81b51535c7c41da9787a4e236a81513c037
Nat Gist <25966140+ngist@users.noreply.github.com>
2025-08-21 17:03:50 +0000

    Update thermostat-animation.js
    Update with my modifications

--- src/thermostat-animation.js
+++ src/thermostat-animation.js
@@ -1,4 +1,5 @@
-// Copyright (c) 2025 by Simone Bernabè (https://codepen.io/simoberny/pen/wrGoZZ)
+// Original work Copyright (c) 2025 by Simone Bernabè (https://codepen.io/simoberny/pen/wrGoZZ)
+// Modified work Copyright (c) 2025 by Nat Gist (https://github.com/ngist/ha_guest_thermostat/)
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 // 
@@ -6,45 +7,36 @@
 //
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-var gradi = 19;
-var max = 34;
-var min = 2;
+const midpoint_temp = (max_setpoint + min_setpoint)/2
+const step_degrees = 320/(max_setpoint-min_setpoint)
 
 function updateGr(){
-  $(".heat").text("" + gradi);
-  $(".ext").text("" + gradi);
-  $(".number").css("transform", "translate(-50%, -50%) rotate("+ (-180 + gradi * 10)+"deg)");
-  $(".shadow").css("transform", "translate(-50%, -50%) rotate("+ (-180 + gradi * 10)+"deg)");
+  $(".main").text("" + setpoint);
+  $(".ext").text("" + setpoint);
+  $(".number").css("transform", "translate(-50%, -50%) rotate("+ ((setpoint-midpoint_temp) * step_degrees)+"deg)");
+  $(".spoint").css("transform", "translate(-50%, -50%) rotate("+ ((setpoint-midpoint_temp) * step_degrees)+"deg)");
   $(".fill").css("animation", "none");
-  $(".shadow").css("animation", "none");
+  $(".spoint").css("animation", "none");
 }
 
-
-$(".minus").mousedown(function(){ 
-  if(gradi > min){
-    gradi--;
+function decrease_setpoint_animation(){ 
     updateGr();
-    if(gradi >= 18){
-      $(".fill1").css("transform", "rotate("+ (gradi - 18) * 10 +"deg)").css("transition-delay", "0s");
-    }else if(gradi == 17){
-      $(".fill2").css("transform", "rotate("+ gradi * 10 +"deg)").css("transition-delay", "0.5s");  
+    if(setpoint >= midpoint_temp){
+      $(".fill1").css("transform", "rotate("+ (setpoint - midpoint_temp) * step_degrees +"deg)").css("transition-delay", "0s");
+    }else if(setpoint == (midpoint_temp-1)){
+      $(".fill2").css("transform", "rotate("+ ((setpoint - min_setpoint) * step_degrees + 20) +"deg)").css("transition-delay", "0.5s");  
     }else{
-      $(".fill2").css("transform", "rotate("+ gradi * 10 +"deg)").css("transition-delay", "0s");
+      $(".fill2").css("transform", "rotate("+ ((setpoint - min_setpoint) * step_degrees + 20) +"deg)").css("transition-delay", "0s");
     }
-  }
-});
+}
 
-$(".plus").mousedown(function(){
-  if(gradi < max){
-    gradi++;
+function increase_setpoint_animation(){
     updateGr();
-    if(gradi > 19){
-      $(".fill1").css("transform", "rotate("+ (gradi - 18) * 10 +"deg)").css("transition-delay", "0s");
-    }else if(gradi == 19){
-      $(".fill1").css("transform", "rotate("+ (gradi - 18) * 10 +"deg)").css("transition-delay", "1s"); 
+    if(setpoint > (midpoint_temp+1)){
+      $(".fill1").css("transform", "rotate("+ (setpoint - midpoint_temp) * step_degrees +"deg)").css("transition-delay", "0s");
+    }else if(setpoint == (midpoint_temp+1)){
+      $(".fill1").css("transform", "rotate("+ (setpoint - midpoint_temp) * step_degrees +"deg)").css("transition-delay", "1s"); 
     }else{
-      $(".fill2").css("transform", "rotate("+ gradi * 10 +"deg)").css("transition-delay", "0s");
+      $(".fill2").css("transform", "rotate("+ ((setpoint - min_setpoint) * step_degrees + 20) +"deg)").css("transition-delay", "0s");
     }
-  }  
-});
-
+}
